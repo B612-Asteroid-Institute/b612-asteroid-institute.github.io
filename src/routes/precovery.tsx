2ab68e551a76431860c96b8bc82496d95e78d0e7
ntellis <nate.tellis@gmail.com>
2022-07-14 13:49:18 -0700

    Remove offending unused import

--- src/routes/precovery.tsx
+++ src/routes/precovery.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from "../components/header"
-import Alert from '@mui/material/Alert';
+// import Alert from '@mui/material/Alert';
 import PrecoveryForm from "../components/precoveryForm"
 import '../css/App.css';
 import '../vendor/bootstrap/css/bootstrap.min.css'
